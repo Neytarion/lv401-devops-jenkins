d30db7c139a7abf6c261282cf3e60d60fe724506
Neytarion <47857375+Neytarion@users.noreply.github.com>
2019-07-10 02:44:27 +0300

    Update compute.tf

--- jenkins/compute.tf
+++ jenkins/compute.tf
@@ -67,7 +67,8 @@ service_account {
  sudo systemctl restart jenkins;
  sleep 50;
  # Getting template from repo
- sudo git clone https://github.com/tooSadman/lv_401 /home/jenkins/lv_401/;
+ sudo git clone https://github.com/Neytarion/lv401-devops-jenkins /home/jenkins/lv_401/;
+ sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job teamcity < /home/jenkins/lv_401/templates/teamcity.xml;
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job up_inst < /home/jenkins/lv_401/templates/up_inst.xml;
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job tomcat < /home/jenkins/lv_401/templates/tomcat.xml;
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job build_db < /home/jenkins/lv_401/templates/database.xml;
