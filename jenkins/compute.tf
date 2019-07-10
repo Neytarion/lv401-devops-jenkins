e88b76a239c2a799230b05031c666b7fd05282e6
Neytarion <47857375+Neytarion@users.noreply.github.com>
2019-07-10 15:53:47 +0300

    Update compute.tf

--- jenkins/compute.tf
+++ jenkins/compute.tf
@@ -68,6 +68,7 @@ sudo gsutil cp -p gs://teamcity-tf/lv401-superproject-89963972f36b.json gs://"te
  sleep 50;
  # ---------------- Getting template from repo
  sudo git clone https://github.com/Neytarion/lv401-devops-jenkins /home/jenkins/lv_401/;
+ sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job terraform < /home/jenkins/lv_401/templates/terraform.xml
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job teamcity < /home/jenkins/lv_401/templates/teamcity.xml;
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job up_inst < /home/jenkins/lv_401/templates/up_inst.xml;
  sudo java -jar /home/jenkins/jenkins-cli.jar -s http://127.0.0.1:8080/ create-job tomcat < /home/jenkins/lv_401/templates/tomcat.xml;
